264af10d532d0d2ba1d6250be4c346c06006db35
Eduardo Silva <eduardosiilva@outlook.com.br>
2025-09-13 18:08:38 -0300

    feat: created excepetions

--- src/main/java/br/com/esotk/my_first_rest_with_java_spring/exception/ExceptionCalculadora.java
+++ src/main/java/br/com/esotk/my_first_rest_with_java_spring/exception/ExceptionCalculadora.java
@@ -1,7 +1,11 @@
 package br.com.esotk.my_first_rest_with_java_spring.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ExceptionCalculadora extends RuntimeException {
+
     public ExceptionCalculadora(String message) {
         super(message);
     }
