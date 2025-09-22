ef3fd39d87277cb29407ca93b6c5d6fcbdbf2fba
Eduardo Silva <eduardosiilva@outlook.com.br>
2025-09-22 11:28:10 -0300

    refactor: formatando Json

--- src/main/java/br/com/esotk/my_first_rest_with_java_spring/data/dto/UserDTO.java
+++ src/main/java/br/com/esotk/my_first_rest_with_java_spring/data/dto/UserDTO.java
@@ -1,11 +1,15 @@
 package br.com.esotk.my_first_rest_with_java_spring.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@JsonPropertyOrder({"id", "nome", "sobrenome", "Genero", "email_pessoal"})
 public class UserDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -13,6 +17,8 @@ public class UserDTO implements Serializable {
     private Long id;
     private String nome;
     private String sobrenome;
+    @JsonProperty("email")
     private String email_pessoal;
+    @JsonIgnore
     private String Genero;
 }
